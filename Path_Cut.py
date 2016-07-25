dc1f7ad65497b85b1c0a877a2a0a4c8a42dcf077
leo <leo@leo-OptiPlex-9020>
2016-07-25 09:52:34 +0800

    Add path cut sample

---
+++ Path_Cut.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+def formatPath(arg):
+	arg = arg[1:].split('/')
+	print arg
+        if len(arg) == 1 and len(arg[0]) != 1 : return arg[0]
+        path = arg[0].upper() + ':/' + '/'.join(arg[1:])
+	return path
+
+arg = '/C/C1/C2/C3'
+
+arg = formatPath(arg)
+
+print arg
+
+
